fbc865cec4243f168a1122156a35264b5bdfe6f1
Asher Bernstein-Meachum <dev.asherbm.untracked946@passinbox.com>
2026-01-01 11:04:54 -0600

    Add documentation; refine scope of modules

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
 mod argparse;
-mod filehandling;
-mod texthandling;
+mod fileio;
+mod text;
 
 use rand::seq::IndexedRandom;
-use std::fs::{self, DirBuilder, File};
-use crate::{argparse::args, filehandling::file, texthandling::text};
-
-fn initialise() {
-    let path: String = format!("{}/{}", file::home_dir_string(), ".config/quoter".to_string());
-    match fs::read_dir(path.clone()) {
-        Ok(_) => (),
-        Err(_) => {
-            DirBuilder::new().recursive(true).create(path.clone()).expect("Internal error: couldn't initialise directory in ~/.config/quoter");
-            File::create(format!("{}/{}", path, "quotes.index")).expect("Internal error: Failed to create index file");
-        },
-    }
-}
+use crate::{argparse::args, fileio::DataFile, text::Quote};
 
 // TODO: Use static &str instead of String
 fn help_text() -> String {
@@ -31,36 +19,36 @@ Options:
 }
 
 fn main() {
-    initialise();
+    fileio::initialise();
 
     match args::parse_args() {
         args::Arg::ArgError(error) => println!("{}", error),
         args::Arg::Help => println!("{}", help_text()),
         args::Arg::Add => {
-            let quote: text::Quote = text::Quote::new_from_input();
-            let quote_file: file::DataFile = file::DataFile::new_quote(quote.title());
+            let quote: Quote = Quote::new_from_input();
+            let quote_file: DataFile = DataFile::new_quote(quote.title());
             quote_file.write(quote.to_file_format());
 
-            let index: file::DataFile = file::DataFile::new_index();
+            let index: DataFile = DataFile::new_index();
             index.write(quote.title());
         },
         args::Arg::Generate => {
-            let index: file::DataFile = file::DataFile::new_index();
+            let index: DataFile = DataFile::new_index();
             let files: String = index.read();
             match files.trim().split("\n").collect::<Vec<&str>>().choose(&mut rand::rng()) {
                 Some(chosen) => {
-                    let quote: file::DataFile = file::DataFile::new_quote(String::from(*chosen));
+                    let quote: DataFile = DataFile::new_quote(String::from(*chosen));
                     println!("{}", quote.read());
                 },
                 None => println!("No files stored. Use quoter --add to add one.")
             };
         },
         args::Arg::List => {
-            let index: file::DataFile = file::DataFile::new_index();
+            let index: DataFile = DataFile::new_index();
             println!("Your stored quotes are:\n{}", index.read())
         },
         args::Arg::Read(title) => {
-            let quote: file::DataFile = file::DataFile::new_quote(title);
+            let quote: DataFile = DataFile::new_quote(title);
             println!("{}", quote.read());
         }
     }
